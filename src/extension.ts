3a3174c5aa5ae90d9b7f432d7b75e177733a3686
hallelujahdrive <hallelujahdrive@gmail.com>
2024-08-20 23:11:22 +0900

    fix: remove settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,3 +1,4 @@
+import type Gio from "gi://Gio";
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import { Kimpanel } from "./kimpanel.js";
 
@@ -5,18 +6,20 @@ import "./stylesheet.css";
 
 export default class PlainExampleExtension extends Extension {
 	private kimpanel: typeof Kimpanel.prototype | null = null;
+	private settings: Gio.Settings | null = null;
 
 	public disable() {
 		this.kimpanel?.destroy();
 		this.kimpanel = null;
+		this.settings = null;
 	}
 
 	public enable() {
 		if (this.kimpanel) return;
 
-		const settings = this.getSettings(
+		this.settings = this.getSettings(
 			"org.gnome.shell.extensions.impanel-with-osk",
 		);
-		this.kimpanel = new Kimpanel(settings, this.dir);
+		this.kimpanel = new Kimpanel(this.settings, this.dir);
 	}
 }
